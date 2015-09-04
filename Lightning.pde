761640b147195a27d19a8834527f3a6e50abb90b
unknown <liwang@697-334-Ethring.sf.k12.internal>
2015-09-04 10:47:44 -0700

    changed code

--- Lightning.pde
+++ Lightning.pde
@@ -1,37 +1,47 @@
-int startX=(int)(Math.random()*300+1);
+int startX=(int)(Math.random()*400+1);
 int startY=0;
 int endX=150;
 int endY=0;
+
+
 void setup()
 {
-  size(300,300);
+  size(400,600);
   strokeWeight(1);
   background(0);
-  
-}
+
+  }
+
 void draw()
 {
-
+	//cloud();
 	int colorR= (int)(Math.random()*256);
 	int colorG= (int)(Math.random()*256);
 	int colorB= (int)(Math.random()*256);
 	stroke(colorR,colorG,colorB);
-	while(endX<300)
+	while(endX<400)
 	{
 	endX=startX+(int)(Math.random()*19-9);
 	endY=startY+(int)(Math.random()*9+1);
 	line(startX,startY,endX,endY);
 	startX=endX;
 	startY=endY;
+	
+
+	
+	}
+	
 }
 
 
-}
+	
+
 void mousePressed()
 {
-	 startX=(int)(Math.random()*300+1);
+	 startX=(int)(Math.random()*400+1);
  	startY=0;
 	 endX=150;
 	endY=0;
+	
 }
 
--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -14,41 +14,51 @@ import java.io.IOException;
 
 public class Lightning extends PApplet {
 
-int startX=(int)(Math.random()*300+1);
+int startX=(int)(Math.random()*400+1);
 int startY=0;
 int endX=150;
 int endY=0;
+
+
 public void setup()
 {
-  size(300,300);
+  size(400,600);
   strokeWeight(1);
   background(0);
-  
-}
+
+  }
+
 public void draw()
 {
-
+	//cloud();
 	int colorR= (int)(Math.random()*256);
 	int colorG= (int)(Math.random()*256);
 	int colorB= (int)(Math.random()*256);
 	stroke(colorR,colorG,colorB);
-	while(endX<300)
+	while(endX<400)
 	{
 	endX=startX+(int)(Math.random()*19-9);
 	endY=startY+(int)(Math.random()*9+1);
 	line(startX,startY,endX,endY);
 	startX=endX;
 	startY=endY;
+	
+
+	
+	}
+	
 }
 
 
-}
+	
+
 public void mousePressed()
 {
-	 startX=(int)(Math.random()*300+1);
+	 startX=(int)(Math.random()*400+1);
  	startY=0;
 	 endX=150;
 	endY=0;
+	
 }
 
   static public void main(String[] passedArgs) {
